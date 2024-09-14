085e8fc978091b7f49db85f6fd9688261075fb12
Noah Telfer <noahtelfer.robert@gmail.com>
2024-09-14 15:55:54 -0400

    Completed the Seriesgit add . !

---
+++ src/components/html/CustomComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Greet } from "../Test/Greet";
+
+
+export const CustomComponent = (props: React.ComponentProps<typeof Greet>) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
